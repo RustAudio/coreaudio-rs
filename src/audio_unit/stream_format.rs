0962369228ec007479292398e3054469d8b8f5e8
mitchmindtree <mitchell.nordine@gmail.com>
2016-01-23 18:02:37 +1100

    Change StreamFormat to more closely match audio unit specification

--- src/audio_unit/stream_format.rs
+++ src/audio_unit/stream_format.rs
@@ -5,24 +5,48 @@
 use bindings::audio_unit as au;
 use error::{self, Error};
 use super::audio_format::AudioFormat;
+use super::SampleFormat;
 
-/// Representation of the AudioStreamBasicDescription.
+/// A representation of the AudioStreamBasicDescription specifically for use with the AudioUnit API.
+///
+/// By using a type specific to the audio unit API, we can remove a lot of unnecessary boilerplate
+/// that is normally associated with the AudioStreamBasicDescription.
+///
+/// Seeing as `LinearPCM` data (the `AudioFormat` used by the `AudioUnit` API) implies a single
+/// frame per packet, we can infer many of the fields in an ASBD from the sample type.
+///
+/// `bytes_per_packet` = size_of::<S>()
+/// `bytes_per_frame` = size_of::<S>()
+/// `frames_per_packet` = 1
+/// `bits_per_channel` = size_of::<S>() * 8
 #[derive(Copy, Clone, Debug)]
 pub struct StreamFormat {
     pub sample_rate: f64,
-    pub audio_format: AudioFormat,
-    pub bytes_per_packet: u32,
-    pub frames_per_packet: u32,
-    pub bytes_per_frame: u32,
+    pub sample_format: SampleFormat,
+    pub flags: super::audio_format::LinearPCMFlags,
     pub channels_per_frame: u32,
-    pub bits_per_channel: u32,
 }
 
 impl StreamFormat {
 
     /// Convert an AudioStreamBasicDescription into a StreamFormat.
-    #[allow(non_snake_case)]
+    ///
+    /// Note: `audio_unit::StreamFormat` exclusively uses the `LinearPCM` `AudioFormat`. This is as
+    /// specified in the documentation:
+    ///
+    /// > Specify kAudioFormatLinearPCM for the mFormatID field. Audio units use uncompressed audio
+    /// data, so this is the correct format identifier to use whenever you work with audio units.
+    ///
+    /// [*Audio Unit Hosting Guide for iOS*](https://developer.apple.com/library/ios/documentation/MusicAudio/Conceptual/AudioUnitHostingGuide_iOS/AudioUnitHostingFundamentals/AudioUnitHostingFundamentals.html)
+    ///
+    /// Returns an `Error` if the `AudioFormat` inferred by the ASBD is not `LinearPCM`.
+    ///
+    /// Returns an `Error` if the sample format type kkkkkkkkkkkkkkkkkkkkkk
     pub fn from_asbd(asbd: au::AudioStreamBasicDescription) -> Result<StreamFormat, Error> {
+        use super::audio_format::linear_pcm_flags;
+
+        const NOT_SUPPORTED: Error = Error::AudioUnit(error::audio_unit::Error::FormatNotSupported);
+
         let au::Struct_AudioStreamBasicDescription {
             mSampleRate,
             mFormatID,
@@ -34,17 +58,24 @@ impl StreamFormat {
             mBitsPerChannel,
             ..
         } = asbd;
+
+        // Retrieve the LinearPCM flags.
+        let flags = match AudioFormat::from_format_and_flag(mFormatID, Some(mFormatFlags)) {
+            Some(AudioFormat::LinearPCM(flags)) => flags,
+            _ => return Err(NOT_SUPPORTED),
+        };
+
+        // Determine the `SampleFormat` to use.
+        let sample_format = match SampleFormat::from_flags_and_bytes_per_frame(flags, mBytesPerFrame) {
+            Some(sample_format) => sample_format,
+            None => return Err(NOT_SUPPORTED),
+        };
+
         Ok(StreamFormat {
             sample_rate: mSampleRate,
-            audio_format: match AudioFormat::from_format_and_flag(mFormatID, Some(mFormatFlags)) {
-                Some(audio_format) => audio_format,
-                None => return Err(Error::AudioUnit(error::audio_unit::Error::FormatNotSupported)),
-            },
-            bytes_per_packet: mBytesPerPacket,
-            frames_per_packet: mFramesPerPacket,
-            bytes_per_frame: mBytesPerFrame,
+            flags: flags,
+            sample_format: sample_format,
             channels_per_frame: mChannelsPerFrame,
-            bits_per_channel: mBitsPerChannel,
         })
     }
 
@@ -52,31 +83,31 @@ impl StreamFormat {
     pub fn to_asbd(self) -> au::AudioStreamBasicDescription {
         let StreamFormat {
             sample_rate,
-            audio_format,
-            bytes_per_packet,
-            frames_per_packet,
-            bytes_per_frame,
+            flags,
+            sample_format,
             channels_per_frame,
-            bits_per_channel,
         } = self;
-        let (format, maybe_flag) = audio_format.to_format_and_flag();
+
+        let (format, maybe_flag) = AudioFormat::LinearPCM(flags).to_format_and_flag();
+
         let flag = maybe_flag.unwrap_or(::std::u32::MAX -2147483647);
+
+        let bytes_per_frame = sample_format.size_in_bytes() as u32;
+        const FRAMES_PER_PACKET: u32 = 1;
+        let bytes_per_packet = bytes_per_frame * FRAMES_PER_PACKET;
+        let bits_per_channel = bytes_per_frame * 8;
+
         au::AudioStreamBasicDescription {
             mSampleRate: sample_rate,
             mFormatID: format,
             mFormatFlags: flag,
             mBytesPerPacket: bytes_per_packet,
-            mFramesPerPacket: frames_per_packet,
+            mFramesPerPacket: FRAMES_PER_PACKET,
             mBytesPerFrame: bytes_per_frame,
             mChannelsPerFrame: channels_per_frame,
             mBitsPerChannel: bits_per_channel,
             mReserved: 0,
         }
-    }
-
-    /// Return the number of channels.
-    pub fn num_channels(&self) -> u32 {
-        self.channels_per_frame
     }
 
 }

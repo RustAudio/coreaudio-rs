0fcc74370919cb6c17a0132a95f4fc402924e4e0
mitchmindtree <mitchell.nordine@gmail.com>
2016-01-23 18:06:54 +1100

    Move callback to its own function to assist with lifetime inference

--- examples/sine.rs
+++ examples/sine.rs
@@ -2,8 +2,8 @@
 
 extern crate coreaudio;
 
-use coreaudio::audio_unit::{AudioUnit, IOType};
-use coreaudio::audio_unit::render_callback::{Args as RenderCallbackArgs, LinearPcmArgs};
+use coreaudio::audio_unit::{AudioUnit, IOType, SampleFormat};
+use coreaudio::audio_unit::render_callback::{self, buffer};
 use std::f64::consts::PI;
 
 
@@ -24,7 +24,7 @@ fn main() {
     run().unwrap()
 }
 
-fn run() -> Result<(), Error> {
+fn run() -> Result<(), coreaudio::Error> {
 
     // 440hz sine wave generator.
     let mut samples = Iter { value: 0.0 }
@@ -33,19 +33,31 @@ fn run() -> Result<(), Error> {
     // Construct an Output audio unit.
     let mut audio_unit = try!(AudioUnit::new(IOType::HalOutput));
 
-    // A function to use as the "render callback".
-    let callback = move |RenderCallbackArgs { format: LinearPcmArgs { buffer }, num_frames, .. }| {
-        for frame in 0..num_frames {
-            let sample = samples.next().unwrap();
-            for channel in buffer.iter_mut() {
-                channel[frame] = sample;
-            }
-        }
-        Ok(())
-    };
+    let stream_format = try!(audio_unit.stream_format());
+    println!("{:#?}", &stream_format);
 
-    try!(audio_unit.set_render_callback(callback));
+    // For this example, our sine wave expects `f32` data.
+    assert!(SampleFormat::F32 == stream_format.sample_format);
+
+    try!(audio_unit.set_render_callback(move |args| callback(args, &mut samples)));
     try!(audio_unit.start());
 
     std::thread::sleep_ms(3000);
+
+    Ok(())
+}
+
+type Args<'a> = render_callback::Args<'a, buffer::LinearPcmInterleaved<'a, f32>>;
+fn callback<'a, I: Iterator<Item=f32>>(args: Args<'a>, samples: &mut I) -> Result<(), ()> {
+    let Args { num_frames, mut buffer, .. } = args;
+    let mut idx = 0;
+    let channels = buffer.data.len() / num_frames;
+    for _ in 0..num_frames {
+        let sample = samples.next().unwrap();
+        for _ in 0..channels {
+            buffer.data[idx] = sample;
+            idx += 1;
+        }
+    }
+    Ok(())
 }

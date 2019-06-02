d9c1a6d4d97f81041b4044f003327d4a35086063
Josh Mcguigan <joshmcg88@gmail.com>
2019-06-02 09:26:17 -0700

    modernize sine example

--- examples/sine.rs
+++ examples/sine.rs
@@ -6,41 +6,49 @@ use coreaudio::audio_unit::{AudioUnit, IOType, SampleFormat};
 use coreaudio::audio_unit::render_callback::{self, data};
 use std::f64::consts::PI;
 
-
-// NOTE: temporary replacement for unstable `std::iter::iterate`
-struct Iter {
-    value: f64,
+struct SineWaveGenerator {
+    time: f64,
+    /// generated frequency in Hz
+    freq: f64,
+    /// magnitude of generated signal
+    volume: f64,
 }
-impl Iterator for Iter {
-    type Item = f64;
-    fn next(&mut self) -> Option<f64> {
-        self.value += 440.0 / 44_100.0;
-        Some(self.value)
+
+impl SineWaveGenerator {
+    fn new(freq: f64, volume: f64) -> Self {
+        SineWaveGenerator {
+            time: 0.,
+            freq,
+            volume,
+        }
     }
 }
 
-
-fn main() {
-    run().unwrap()
+impl Iterator for SineWaveGenerator {
+    type Item = f32;
+    fn next(&mut self) -> Option<f32> {
+        self.time += 1. / 44_100.;
+        let output = ((self.freq * self.time * PI * 2.).sin() * self.volume) as f32;
+        Some(output)
+    }
 }
 
-fn run() -> Result<(), coreaudio::Error> {
-
-    // 440hz sine wave generator.
-    let mut samples = Iter { value: 0.0 }
-        .map(|phase| (phase * PI * 2.0).sin() as f32 * 0.15);
+fn main() -> Result<(), coreaudio::Error> {
+    let frequency_hz = 440.;
+    let volume = 0.15;
+    let mut samples = SineWaveGenerator::new(frequency_hz, volume);
 
     // Construct an Output audio unit that delivers audio to the default output device.
-    let mut audio_unit = try!(AudioUnit::new(IOType::DefaultOutput));
+    let mut audio_unit = AudioUnit::new(IOType::DefaultOutput)?;
 
-    let stream_format = try!(audio_unit.output_stream_format());
+    let stream_format = audio_unit.output_stream_format()?;
     println!("{:#?}", &stream_format);
 
     // For this example, our sine wave expects `f32` data.
     assert!(SampleFormat::F32 == stream_format.sample_format);
 
     type Args = render_callback::Args<data::NonInterleaved<f32>>;
-    try!(audio_unit.set_render_callback(move |args| {
+    audio_unit.set_render_callback(move |args| {
         let Args { num_frames, mut data, .. } = args;
         for i in 0..num_frames {
             let sample = samples.next().unwrap();
@@ -49,8 +57,8 @@ fn run() -> Result<(), coreaudio::Error> {
             }
         }
         Ok(())
-    }));
-    try!(audio_unit.start());
+    })?;
+    audio_unit.start()?;
 
     std::thread::sleep(std::time::Duration::from_millis(3000));
 
